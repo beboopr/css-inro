f05447911a2102323ba20d4688f796d07c6e096b
beboopr <linnets-router.09@icloud.com>
2022-10-24 15:51:32 -0400

    intro-css

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const family = 
+        {
+            name: 'Ed',
+            job: 'Front End Dev',
+        }
+        console.log('hey class heres my', family)
+        document.getElementById('Contact').style.color = 'purple'
+        
+        const handleColorChange = () => {
+            console.log('changing color')
+        }
+
+        const handleAlert = () => {
+            alert('Alerting the class')
+        }
